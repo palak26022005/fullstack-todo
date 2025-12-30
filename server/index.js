9472f60c51ecbead899a15a79828adedc1d0fb6d
palak26022005 <162999811+palak26022005@users.noreply.github.com>
2025-12-30 13:25:28 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -41,5 +41,6 @@ app.delete("/api/todos/:id", (req, res) => {
   res.status(204).send();
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
